b623a2b6910bc55646203bd0047f037d022fd719
Robert1192 <robertapostol15@gmail.com>
2026-06-18 12:14:17 +0200

    3

--- ProjectWebsite10032026/obj/Debug/net8.0/ProjectWebsite10032026.AssemblyInfo.cs
+++ ProjectWebsite10032026/obj/Debug/net8.0/ProjectWebsite10032026.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectWebsite10032026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ddef1c9e624cce053fbe80369cbb5517bd12b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2170d1cdb051e1733fc29f315ac32dc283e9e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectWebsite10032026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectWebsite10032026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
